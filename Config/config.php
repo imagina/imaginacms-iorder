c056eb87c9f0b0409f86949507f019bd02b54ad6
nicolas-montealegre <nmontealegre@contractor.renuityhome.com>
2024-12-10 00:11:56 -0500

    Added: synchronizable orders in Iorder

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,37 @@
 <?php
 
 return [
+  'name' => 'Iorder',
+  'frontendModuleName' => 'qorder',
 
+  'synchronizable' => [
+    'entities' => [
+      'iorder_syncOrders' => [
+        'base_template_id' => '1LeggrpfqCiM8f7MnK5mSMWuqFZonm-WcpAP7zWs5r1k',
+        'apiRoute' => '/iorder/v3/orders',
+        "supportedActions" =>  ["import"],
+        'sheetName' => 'Orders',
+        'include' => '',
+        'customColumns' => true,
+        'dependencies' => [
+          'iorder_syncProvider' => [
+            'apiRoute' => '/profile/v1/users',
+            'sheetName' => 'Provider',
+            'columns' => [
+              'id' => 'ID',
+              'fullName' => 'NOMBRE'
+            ]
+          ],
+          'iorder_syncProduct' => [
+            'apiRoute' => '/iproduct/v1/products',
+            'sheetName' => 'Product',
+            'columns' => [
+              'id' => 'ID',
+              'title' => 'NOMBRE'
+            ]
+          ]
+        ]
+      ]
+    ]
+  ],
 ];
