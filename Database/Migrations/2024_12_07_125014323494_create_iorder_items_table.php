6b806be6de6952b3330999d3fa333cb73684ba76
nicolas-montealegre <nmontealegre@contractor.renuityhome.com>
2024-12-10 19:23:51 -0500

    Fixed: fields in Item Entity

--- Database/Migrations/2024_12_07_125014323494_create_iorder_items_table.php
+++ Database/Migrations/2024_12_07_125014323494_create_iorder_items_table.php
@@ -19,15 +19,15 @@ return new class extends Migration {
       $table->integer('order_id')->unsigned();
       $table->foreign('order_id')->references('id')->on('iorder__orders')->onDelete('cascade');
       $table->integer('status_id')->default(Status::ITEM_PENDING);
-      $table->string('entity_type');
-      $table->integer('entity_id');
+      $table->string('entity_type')->nullable();
+      $table->integer('entity_id')->nullable();
       $table->string('title')->nullable();
       $table->integer('quantity')->nullable();
       $table->float('price', 20, 2)->default(0);
       $table->float('total', 50, 2)->default(0);
       $table->string('options')->nullable();
       $table->integer('discount')->nullable();
-      $table->string('extra_data')->nullable();
+      $table->text('extra_data')->nullable();
       // Audit fields
       $table->timestamps();
       $table->auditStamps();
