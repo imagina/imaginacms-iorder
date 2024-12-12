02076e3f4dc78c67adbb3a7b44d60da2f3a41142
nicolas-montealegre <nmontealegre@contractor.renuityhome.com>
2024-12-11 22:28:17 -0500

    Added: translations about order

---
+++ Resources/lang/es/cms.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+  "sidebar" => [
+    "adminGroup" => "Órdenes",
+    "adminOrders" => "Órdenes",
+    "adminSupplies" => "Suministros",
+  ],
+  "label" => [
+    "refuse" => "Rechazar",
+    "refuseOrder" => "Rechazar Orden",
+    "refuseMessage" => "¿Deseas rechazar permanentemente esta orden? Una vez que se rechace, no habrá vuelta atrás. Por favor, asegúrate.",
+    "accept" => "Aceptar",
+    "acceptOrder" => "Aceptar Orden",
+    "acceptMessage" => "¿Deseas aceptar esta orden?. Por favor, asegúrate que las unidades y el precio sean los correctos.",
+    "suppliesHelp" => "Este módulo te permite gestionar órdenes=> revisa, edita cantidades o precios si es necesario, y decide si aceptarlas o rechazarlas. Asegúrate de verificar los detalles antes de confirmar."
+  ],
+  "form" => [
+    "order" => "Orden",
+    "supplier" => "Proveedor",
+    "price" => "Precio",
+    "supplierPrice" => "Precio Proveedor",
+    "requestedQuantity" => "Unidades Solicitadas",
+    "avaliableQuantity" => "Unidades Disponibles",
+    "requestedAndAvaliableQuantity" => "Unidades Solicitadas/Disponibles",
+    "observations" => "Observaciones",
+    "beforeUpdate" => [
+      "invalidPrice" => "Ingrese un precio valido.",
+      "invalidQuantity" => "Ingrese una cantidad valida.",
+      "invalidAvaliableQuantity" => "La cantidad de unidades disponibles debe ser menor a la solicitada."
+    ]
+  ],
+  "orderManagement" => "Órdenes",
+  "suppliesManagement" => "Suministros",
+];
